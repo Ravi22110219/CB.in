80e14ddd97f36ad0f296149c407d932dfef98687
ravi22110219 <ravi.kumawat@iitgn.ac.in>
2025-03-22 21:06:32 +0530

    intial

---
+++ app/(root)/page.tsx
@@ -0,0 +1,24 @@
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))\  // await delay(2000)
+import ProductList from '@/components/shared/product/product-list'
+import { getLatestProduct } from '@/lib/actions/product.actions'
+
+// export const metadata = {
+
+//   title: 'Home',
+// }
+
+const Homepage = async () => {
+  const latestProducts = await getLatestProduct()
+
+  return (
+    <section className="p-9">
+      {/* <ProductList
+        data={latestProducts}
+        title="Newest Arrivals"
+        limit={4}
+      ></ProductList> */}
+    </section>
+  )
+}
+
+export default Homepage
